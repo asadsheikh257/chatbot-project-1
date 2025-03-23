e0d1dec35d436dd3dd450002e0cc63324eb12d07
Asad Sheikh <asadsheikh257@gmail.com>
2025-03-24 03:00:13 +0500

    update url

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/chat/"  # Adjust if running FastAPI on a different host
+API_URL = "https://chatbot-project-1-production.up.railway.app/"  # Adjust if running FastAPI on a different host
 
 st.title("Mistral AI Chatbot")
 
